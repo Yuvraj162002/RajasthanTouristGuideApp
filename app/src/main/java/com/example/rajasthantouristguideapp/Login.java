64625a7e582a3d9fcf75b7b7e4bc0d78de6fa761
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2022-04-19 01:13:19 +0530

    INITAL COMMIT

---
+++ app/src/main/java/com/example/rajasthantouristguideapp/Login.java
@@ -0,0 +1,33 @@
+package com.example.rajasthantouristguideapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+         button = (Button) findViewById(R.id.signup);
+
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(Login.this,MainActivity.class);
+               startActivity(intent);
+           }
+       });
+
+
+
+
+
+    }
+}
